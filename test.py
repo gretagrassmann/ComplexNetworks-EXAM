3b7c2c70f6c2c5b5a5c89d908149c654b7eb7ec0
Greta Grassmann <gretagrassmann0@gmail.com>
2020-11-20 15:02:27 +0100

    Version with Cycles.

--- test.py
+++ test.py
@@ -12,7 +12,8 @@ start_time = time.time()
 print("NO EDGE USED")
 if __name__=='__main__':
 
-  model_num = 89
+  #model_num = 44
+  n = [298]
   #load the testing data
   test_data_file = os.path.join(
       'C:\\Users\\Cobal\\Desktop\\ComplexNetworksEXAM\\Graph_convolution_with_proteins-master\\data_SimpleVersion',
@@ -31,48 +32,49 @@ if __name__=='__main__':
   saver = tf.train.Saver()
   with tf.Session() as sess:
      # set up tensorflow session
-     saver.restore(sess, './saved_models/model_%d.ckpt'%(model_num))
-     print(" Using model %d "%(model_num)," for testing %d proteins"%(len(test_data)))
+     for model_num in n:
+         saver.restore(sess, './saved_models/model_%d.ckpt'%(model_num))
+         print(" Using model %d "%(model_num)," for testing %d proteins"%(len(test_data)))
 
-     all_preds = []
-     all_labels = []
-     all_losses = []
-     count = 0
-     
-     
-     for prot_data in test_data:
-       temp_data = copy.deepcopy(prot_data)
-       n = prot_data['label'].shape[0] #no of labels for this protein molecule.
-       count = count +1
-       print("Protein number = %d" %count)
-       print("Number of residues couples = %d" %n)
-       #split the labels into chunks of minibatch_size.
-       batch_split_points = np.arange(0,n,minibatch_size)[1:]
-       batches = np.array_split(prot_data['label'],batch_split_points)
-       for a_batch in batches:
-         temp_data['label'] = a_batch     
-         feed_dict = build_feed_dict(model_variables_list, temp_data)
-         res = sess.run([loss,preds,labels], feed_dict=feed_dict)
-         pred_v = np.squeeze(res[1])
-         if len(pred_v.shape)==0:
-           pred_v = [pred_v]
-           all_preds += pred_v
-         else:
-           pred_v = pred_v.tolist()
-           all_preds += pred_v
-         all_labels += res[2].tolist()
-         all_losses += [res[0]]
+         all_preds = []
+         all_labels = []
+         all_losses = []
+         count = 0
 
 
+         for prot_data in test_data:
+           temp_data = copy.deepcopy(prot_data)
+           n = prot_data['label'].shape[0] #no of labels for this protein molecule.
+           count = count +1
+           print("Protein number = %d" %count)
+           print("Number of residues couples = %d" %n)
+           #split the labels into chunks of minibatch_size.
+           batch_split_points = np.arange(0,n,minibatch_size)[1:]
+           batches = np.array_split(prot_data['label'],batch_split_points)
+           for a_batch in batches:
+             temp_data['label'] = a_batch
+             feed_dict = build_feed_dict(model_variables_list, temp_data)
+             res = sess.run([loss,preds,labels], feed_dict=feed_dict)
+             pred_v = np.squeeze(res[1])
+             if len(pred_v.shape)==0:
+               pred_v = [pred_v]
+               all_preds += pred_v
+             else:
+               pred_v = pred_v.tolist()
+               all_preds += pred_v
+             all_labels += res[2].tolist()
+             all_losses += [res[0]]
 
-     fpr, tpr, _ = roc_curve(all_labels, all_preds)
-     roc_auc = auc(fpr, tpr)
-     print('test mean loss = ',np.mean(all_losses))
-     print('test roc_auc = ',roc_auc)
-     with open("Testing_loss_noedge.txt","a+") as f:
-         if model_num == 0:
-             f.write('Average loss, model number, roc_auc \n')
-         f.write(str(np.mean(all_losses))+','+ str(model_num)+','+str(roc_auc)+"\n")
+
+
+         fpr, tpr, _ = roc_curve(all_labels, all_preds)
+         roc_auc = auc(fpr, tpr)
+         print('test mean loss = ',np.mean(all_losses))
+         print('test roc_auc = ',roc_auc)
+         with open("CYCLE_Testing_loss_noedge.txt","a+") as f:
+             if model_num == 0:
+                 f.write('Average loss, model number, roc_auc \n')
+             f.write(str(np.mean(all_losses))+','+ str(model_num)+','+str(roc_auc)+"\n")
 
 
 print("My program took", time.time() - start_time, "to run")
